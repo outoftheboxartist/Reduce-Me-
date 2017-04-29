ad1abe2979dbbe6bc7ae13f6d66fa6d2b895c599
JoseThen <thenj@ymail.com>
2017-04-29 11:35:26 -0400

    Added Static and Tempalte folders and added some more html tags, added a template page named layout

--- reduce.py
+++ reduce.py
@@ -1,6 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template as thing, redirect, request, url_for, abort, flash, session
+
 app = Flask(__name__)
 
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return thing('index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+ 	app.run(debug=True, host='0.0.0.0')
