30f259811ea4dc0219cae6ae13b9f04f27a1cdb7
JoseThen <thenj@ymail.com>
2017-05-29 22:49:29 -0400

    Fixed up Dropzone a bit more look at the JS file, I also stopped ffmpeg for now, we need to have the workflow finished, after uploading it is not moving to new page, will fix later

--- reduce.py
+++ reduce.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = 'video/'
 
-app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mov', 'mp4,' , 'tf', 'wmv', 'avi', 'flv'])
-
-
+app.config['ALLOWED_EXTENSIONS'] = set(
+    ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mov', 'mp4,', 'tf', 'wmv', 'avi', 'flv'])
 
 
 @app.route('/')
 def index():
-    return thing('index.html')
+    return thing('main.html')
 
 # Testing Upload JS ===================================
+
 
 @app.route('/test')
 def test():
@@ -32,7 +32,7 @@ def send_file():
     save_path = "{}/{}".format(app.config["UPLOAD_FOLDER"], filename)
     fileob.save(save_path)
 
-      # open and close to update the access time.
+    # open and close to update the access time.
     with open(save_path, "r") as f:
         pass
 
@@ -57,6 +57,8 @@ def get_filenames():
 # =====================================================
 
 # Route that will process the file upload
+
+
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     # Get the name of the uploaded file
@@ -68,28 +70,30 @@ def upload():
         # file.save(filename)
         # Move the file form the temporal folder to
         # the upload folder we setup
-        print 'gotfile'
+        print 'The File has been named'
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         # Redirect the user to the uploaded_file route, which
         # will basicaly show on the browser the uploaded file
-        print 'gotfile2222222222222222222222222222222222222222'
-        return peggy(filename)
-        # return redirect(url_for("compress", filename = filename))
+        print 'File Has been saved '
+        #return peggy(filename) This will process the file
+        return redirect(url_for("compress", filename = filename))
 
     print 'Peggy Did not Run Son'
-                               
+
 
 def peggy(filename):
-	output_name = changey(filename)
-	subprocess.call('ffmpeg -i '+str(filename)+' -b 1000000 '+output_name, cwd=os.path.join(app.config['UPLOAD_FOLDER']), shell=True)
-	return redirect(url_for("compress", filename=output_name))
-	# subprocess.call('ffmpeg -r 10 -i frame%03d.png -r ntsc '+str(out_movie), shell=True)
+    output_name = changey(filename)
+    subprocess.call('ffmpeg -i ' + str(filename) + ' -b 1000000 ' + output_name,
+                    cwd=os.path.join(app.config['UPLOAD_FOLDER']), shell=True)
+    return redirect(url_for("compress", filename=output_name))
+    # subprocess.call('ffmpeg -r 10 -i frame%03d.png -r ntsc '+str(out_movie), shell=True)
+
 
 def changey(filename):
-	file = str(filename)
-	splitter = file.split(".")
-	rs = splitter[0][::-1]+"."+splitter[1]
-	return rs
+    file = str(filename)
+    splitter = file.split(".")
+    rs = splitter[0][::-1] + "." + splitter[1]
+    return rs
 
 
 @app.route('/uploads/<filename>')
@@ -98,14 +102,9 @@ def uploaded_file(filename):
                                filename)
 
 
-
 @app.route('/compress/<filename>')
 def compress(filename):
-    return thing("compress.html", filename = filename)
-
-
-
-
+    return thing("compress.html", filename=filename)
 
 
 # For a given file, return whether it's an allowed type or not
@@ -113,14 +112,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
-
-
-
-
-
 if __name__ == '__main__':
 
- 	app.run(debug=True, host='0.0.0.0')
- 	
- 	
-
+    app.run(debug=True, host='0.0.0.0')
